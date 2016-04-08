359b112bf06f4aba25196e77e92b5bba3306bb45
Jannick Fahlbusch <git@jf-projects.de>
2016-04-08 20:19:06 +0200

    Implement the functions

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 	"net"
@@ -16,10 +17,31 @@ var (
 	bindTo = flag.String("bindTo", ":13337", "Address and port to listen on, defaults to 0.0.0.0:13337")
 )
 
+/*
+type LighterServer interface {
+	SetColor(context.Context, *ColorMessage) (*Confirmation, error)
+	CheckConnection(context.Context, *InitMessage) (*ColorMessage, error)
+	SwitchState(context.Context, *StateMessage) (*Confirmation, error)
+}
+*/
 type server struct{}
 
 func (s *server) SetColor(ctx context.Context, colorMessage *LighterGRPC.ColorMessage) (*LighterGRPC.Confirmation, error) {
 	dioder.SetAll(uint8(colorMessage.R), uint8(colorMessage.G), uint8(colorMessage.B))
+
+	return &LighterGRPC.Confirmation{true}, nil
+}
+
+func (s *server) CheckConnection(ctx context.Context, initMessage *LighterGRPC.InitMessage) (*LighterGRPC.ColorMessage, error) {
+	return nil, errors.New("CheckConnection is currently not implemented")
+}
+
+func (s *server) SwitchState(ctx context.Context, stateMessage *LighterGRPC.StateMessage) (*LighterGRPC.Confirmation, error) {
+	if stateMessage.Onstate {
+		dioder.SetAll(255, 255, 255)
+	} else {
+		dioder.SetAll(0, 0, 0)
+	}
 
 	return &LighterGRPC.Confirmation{true}, nil
 }
