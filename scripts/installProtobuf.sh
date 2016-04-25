b9a9046a82f381a582248c6ff9df530a2d77847e
Jannick Fahlbusch <git@jf-projects.de>
2016-04-25 17:52:36 +0200

    Hopefully a final fix. :(

--- scripts/installProtobuf.sh
+++ scripts/installProtobuf.sh
@@ -2,5 +2,4 @@
 set -ex
 wget  https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
 tar -xzvf *.tar.gz
-cd *protobuf* && ./configure --prefix=/usr && make && sudo make 
-install
+cd *protobuf* && ./configure --prefix=/usr && make && sudo make install
