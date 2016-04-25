c923299531c46bbca051b65de298c2d6bab10840
Jannick Fahlbusch <git@jf-projects.de>
2016-04-25 17:23:23 +0200

    Add Travis-CI

---
+++ scripts/installProtobuf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -ex
+wget  https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
+tar -xzvf *.tar.gz
+cd *protobuf* && ./configure --prefix=/usr && make && sudo 
+make install
