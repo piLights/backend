d55485494e50569a06197f495c748928c07fc98d
Jannick Fahlbusch <git@jf-projects.de>
2016-04-26 21:04:41 +0200

    Added a debug-mode

--- src/flags.go
+++ src/flags.go
@@ -7,4 +7,5 @@ var (
 	redPin   = kingpin.Flag("red", "Number of the red-pin").Default("18").OverrideDefaultFromEnvar("dioderRedPin").Short('r').Int()
 	greenPin = kingpin.Flag("green", "Number of the green-pin").Default("4").OverrideDefaultFromEnvar("dioderGreenPin").Short('g').Int()
 	bluePin  = kingpin.Flag("blue", "Number of the blue-pin").Default("17").OverrideDefaultFromEnvar("dioderBluePin").Short('b').Int()
+	debug    = kingpin.Flag("debug", "Debug-mode").Bool()
 )
--- src/main.go
+++ src/main.go
@@ -18,6 +18,10 @@ import (
 type server struct{}
 
 func (s *server) SetColor(ctx context.Context, colorMessage *LighterGRPC.ColorMessage) (*LighterGRPC.Confirmation, error) {
+	if *debug {
+		log.Println("SetColor:", colorMessage)
+	}
+
 	colorSet := color.RGBA{uint8(colorMessage.R), uint8(colorMessage.G), uint8(colorMessage.B), uint8(colorMessage.Opacity)}
 
 	dioder.SetAll(colorSet)
@@ -26,6 +30,10 @@ func (s *server) SetColor(ctx context.Context, colorMessage *LighterGRPC.ColorMe
 }
 
 func (s *server) CheckConnection(ctx context.Context, initMessage *LighterGRPC.InitMessage) (*LighterGRPC.ColorMessage, error) {
+	if *debug {
+		log.Println("CheckConnection", initMessage)
+	}
+
 	colorSet := dioder.GetCurrentColor()
 
 	onstate := true
@@ -34,10 +42,17 @@ func (s *server) CheckConnection(ctx context.Context, initMessage *LighterGRPC.I
 		onstate = false
 	}
 
+	if *debug {
+		log.Println("CheckConnection: Returning the current settings:", colorSet)
+	}
 	return &LighterGRPC.ColorMessage{onstate, int32(colorSet.R), int32(colorSet.G), int32(colorSet.B), int32(colorSet.A), "Dioder-Server"}, nil
 }
 
 func (s *server) SwitchState(ctx context.Context, stateMessage *LighterGRPC.StateMessage) (*LighterGRPC.Confirmation, error) {
+	if *debug {
+		log.Println("SwitchState", stateMessage)
+	}
+
 	if stateMessage.Onstate {
 		dioder.SetAll(color.RGBA{255, 255, 255, 255})
 	} else {
@@ -52,8 +67,14 @@ func main() {
 	kingpin.Parse()
 
 	//Set the pins
+	if *debug {
+		log.Printf("Configuring the Pins to: Red: %d, Green: %d, Blue: %d\n", *redPin, *greenPin, *bluePin)
+	}
 	dioder.SetPins(*redPin, *greenPin, *bluePin)
 
+	if *debug {
+		log.Printf("Binding to %s", *bindTo)
+	}
 	listener, error := net.Listen("tcp", *bindTo)
 	if error != nil {
 		log.Fatalf("failed to listen: %v", error)
