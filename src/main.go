d9a70c5027f6fe56bd22ed938cec5a6ad9f99fdd
Jannick Fahlbusch <git@jf-projects.de>
2016-04-26 21:56:09 +0200

    Give the server-struct an own file

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 	"net"
 	"runtime"
@@ -10,61 +9,12 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/piLights/dioder"
-	"golang.org/x/net/context"
 
 	LighterGRPC "github.com/piLights/dioder-rpc/src/proto"
 	"google.golang.org/grpc"
 )
 
 const version = "debugVersion"
-
-//server implements the server-interface required by GRPC
-type server struct{}
-
-func (s *server) SetColor(ctx context.Context, colorMessage *LighterGRPC.ColorMessage) (*LighterGRPC.Confirmation, error) {
-	if *debug {
-		log.Println("SetColor:", colorMessage)
-	}
-
-	colorSet := color.RGBA{uint8(colorMessage.R), uint8(colorMessage.G), uint8(colorMessage.B), uint8(colorMessage.Opacity)}
-
-	dioder.SetAll(colorSet)
-
-	return &LighterGRPC.Confirmation{true}, nil
-}
-
-func (s *server) CheckConnection(ctx context.Context, initMessage *LighterGRPC.InitMessage) (*LighterGRPC.ColorMessage, error) {
-	if *debug {
-		log.Println("CheckConnection", initMessage)
-	}
-
-	colorSet := dioder.GetCurrentColor()
-
-	onstate := true
-
-	if colorSet.R == 0 && colorSet.G == 0 && colorSet.B == 0 {
-		onstate = false
-	}
-
-	if *debug {
-		log.Println("CheckConnection: Returning the current settings:", colorSet)
-	}
-	return &LighterGRPC.ColorMessage{onstate, int32(colorSet.R), int32(colorSet.G), int32(colorSet.B), int32(colorSet.A), "Dioder-Server"}, nil
-}
-
-func (s *server) SwitchState(ctx context.Context, stateMessage *LighterGRPC.StateMessage) (*LighterGRPC.Confirmation, error) {
-	if *debug {
-		log.Println("SwitchState", stateMessage)
-	}
-
-	if stateMessage.Onstate {
-		dioder.SetAll(color.RGBA{255, 255, 255, 255})
-	} else {
-		dioder.SetAll(color.RGBA{})
-	}
-
-	return &LighterGRPC.Confirmation{true}, nil
-}
 
 func main() {
 	kingpin.Version(version)
