6f725a09387fe57061424415d924c70c79c6be1d
Jannick Fahlbusch <git@jf-projects.de>
2016-05-06 11:56:49 +0200

    TRy to fetch the prebuilt protoc-binaries

--- scripts/installProtobuf.sh
+++ scripts/installProtobuf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -ex
-wget  https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
-tar -xzvf *.tar.gz
-cd *protobuf* && ./configure --prefix=/usr && make && sudo make install
+#wget  https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
+#tar -xzvf *.tar.gz
+#cd *protobuf* && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protoc-3.0.0-beta-2-linux-x86_64.zip
+unzip protoc-3.0.0-beta-2-linux-x86_64.zip
+sudo cp protoc /usr/bin
