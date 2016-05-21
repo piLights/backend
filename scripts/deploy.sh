81d413a7b02b29f127cb22fa837b25ae61ca8393
Jannick Fahlbusch <git@jf-projects.de>
2016-05-21 16:18:43 +0200

    Move the deploying into an own file

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin//bash
+
+cd ../src/
+gox -output "../dist/dioderAPI_{{.OS}}_{{.Arch}}" -parallel=2 -verbose -os="${OPERATING_SYSTEM}"
+cd ..
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	ghr --username piLights --token $GITHUB_TOKEN --replace --prerelease --debug pre-release dist/
+fi
