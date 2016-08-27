5814a3876b70fcc08c118eb25bc34fa711684ab7
Jannick Fahlbusch <git@jf-projects.de>
2016-08-27 11:01:00 +0200

    Use protobuf 3.0.0

--- scripts/installProtobuf.sh
+++ scripts/installProtobuf.sh
@@ -3,6 +3,6 @@ set -ex
 #wget  https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
 #tar -xzvf *.tar.gz
 #cd *protobuf* && ./configure --prefix=/usr && make && sudo make install
-wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-3/protoc-3.0.0-beta-3-linux-x86_64.zip
-unzip protoc-3.0.0-beta-3-linux-x86_64.zip
+wget https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+unzip protoc-3.0.0-linux-x86_64.zip
 sudo cp protoc /usr/bin
