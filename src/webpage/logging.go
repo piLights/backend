b1d7f0c11b1d1ea0e6e13282bab6ff0ee60a9d53
Jannick Fahlbusch <git@jf-projects.de>
2016-10-13 22:46:18 +0200

    Log to the unified log in debug mode

--- src/webpage/logging.go
+++ src/webpage/logging.go
@@ -12,12 +12,14 @@ func Logging(inner http.Handler, name string) http.Handler {
 
 		inner.ServeHTTP(w, r)
 
-		log.Printf(
-			"%s\t%s\t%s\t%s",
-			r.Method,
-			r.RequestURI,
-			name,
-			time.Since(start),
-		)
+		if dioder.Debug {
+			logChan <- log.Sprintf(
+				"%s\t%s\t%s\t%s",
+				r.Method,
+				r.RequestURI,
+				name,
+				time.Since(start),
+			)
+		}
 	})
 }
