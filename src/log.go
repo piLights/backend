b866c79d6a761304877551b9b5f5a0fd86e879b3
Jannick Fahlbusch <git@jf-projects.de>
2016-10-13 20:09:49 +0200

    Limit the size of the log to sizeOf(int32)

--- src/log.go
+++ src/log.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"time"
 
 	"gitlab.com/piLights/proto"
 )
+
+const maxSliceSize = math.MaxInt32
 
 type logEntryList struct {
 	EntryList []*LighterGRPC.LogEntry
@@ -44,6 +47,13 @@ func loggingService(logChan, fatalChan chan interface{}) {
 }
 
 func saveLog(line interface{}) {
+	// Check if the push of another entry would cause the EntryList-Slice to overflow
+	if logList.Count+1 > maxSliceSize {
+		// Remove the first item
+		logList.EntryList = append(logList.EntryList[:0], logList.EntryList[1:]...)
+		logList.Count--
+	}
+
 	entry := &LighterGRPC.LogEntry{
 		Time:    time.Now().UnixNano(),
 		Message: fmt.Sprintf("%v", line), // Fix: Type assertions failing here: We have string and *grpc.error
