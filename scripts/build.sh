a3930ea42eb53b9fd7ba9f8a678897f2100bc8fd
Jannick Fahlbusch <git@jf-projects.de>
2016-11-21 20:46:53 +0100

    Change the output-path of gox

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gox -output "../dist/dioderAPI_{{.OS}}_{{.Arch}}" -parallel=2 -verbose -os="${OPERATING_SYSTEM}" -ldflags="-s -w"
+gox -output "dist/dioderAPI_{{.OS}}_{{.Arch}}" -parallel=2 -verbose -os="${OPERATING_SYSTEM}" -ldflags="-s -w"
